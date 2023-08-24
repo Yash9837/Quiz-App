f5e8f3103e0711353fc74b8a204f2be6c7303675
Yash Gupta <113465469+Yash9837@users.noreply.github.com>
2023-08-24 15:28:56 +0530

    added quiz website

---
+++ question.js
@@ -0,0 +1,45 @@
+let questions = [
+    {
+        numb: 1,
+        questions: "Which is the Indian Car of the Year 2019?",
+        answer: "C. Maruti Suzuki Swift",
+        options: [
+        "A. Hyundai Santro",
+        "B. Maruti Suzuki Ertiga",
+        "C. Maruti Suzuki Swift",
+        "D. None of these"
+
+        ]
+    },
+    {
+        numb: 2,
+        questions: "What is the slogan of Audi Car?",
+        answer: "B. “Being Ahead through Technology”",
+        options: ["A. “We are the competition”",
+        "B. “Being Ahead through Technology”",
+        "C. “Moving forward”",
+        "D. None of these"
+
+        ]
+    },
+    {
+        numb: 3,
+        questions: "Motor City is the nickname for which American city?",
+        answer: "B. Detroit",
+        options: ["A. Chicago",
+        "B. Detroit",
+        "C. Michigan",
+        "D. None of these"
+        ]
+    },
+    {
+        numb: 4,
+        questions: "Which of the following is not Jaguar model?",
+        answer: "A. TL",
+        options: ["A. TL",
+        "B. XF",
+        "C. XJ",
+        "D. None of these"
+        ]
+    },
+]
